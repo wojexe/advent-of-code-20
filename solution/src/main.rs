26618a2a8e8d5d515fcfbe5484fe3f058cfa44dd
wojexe <21208490+wojexe@users.noreply.github.com>
2020-12-02 21:30:06 +0100

    Day 1 solution (second star)

--- solution/src/main.rs
+++ solution/src/main.rs
@@ -1,26 +1,28 @@
-use std::io::{self, BufRead};
 use std::collections::HashMap;
-// use std::prelude::*;
+use std::io::{self, BufRead};
 
 fn main() -> io::Result<()> {
-  // let mut input: Vec<i32> = Vec::new();
   let mut input: HashMap<i32, Result<(), ()>> = HashMap::new();
 
   let stdin = io::stdin();
 
-  // println!("\nYou've enetered: ");
   for ref line in stdin.lock().lines() {
     if let Ok(s) = &line {
-      if *s == "" { break; }
-      // else { input.push( s.as_str().parse().unwrap() ) }
-      else { input.insert( s.as_str().parse().unwrap(), Ok(()) ); }
+      if *s == "" {
+        break;
+      }
+      else {
+        input.insert(s.as_str().parse().unwrap(), Ok(()));
+      }
     }
   }
 
   for x in input.keys() {
-    if input.contains_key(&(2020 - x)) {
-      println!("Answer: {}", x * (2020-x));
-      return Ok(())
+    for y in input.keys() {
+      if input.contains_key(&(2020 - x - y)) {
+        println!("Answer: {}", x * y * (2020 - x - y));
+        return Ok(());
+      }
     }
   }
 
