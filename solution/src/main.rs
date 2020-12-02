5bdce82c4b8b82ca3d0c62fc0eb93c15d2aa96bf
wojexe <21208490+wojexe@users.noreply.github.com>
2020-12-02 23:15:55 +0100

    Day 2 solution (second star)

--- solution/src/main.rs
+++ solution/src/main.rs
@@ -6,10 +6,10 @@ fn main() {
 
   let mut valid_count: u16 = 0;
 
-  let mut min: u16 = 0;
-  let mut max: u16 = 0;
-  let mut ch: char = '0';
-  let mut string: String = String::new();
+  let mut min: u16;
+  let mut max: u16;
+  let mut ch: char;
+  let mut string: String;
 
   for line in stdin.lock().lines() {
 
@@ -21,17 +21,26 @@ fn main() {
     
     scan!(l.bytes() => "{}-{} {}: {}", min, max, ch, string);
 
-    let mut ctr: u16 = 0;
+    // let mut ctr: u16 = 0;
   
-    for c in string.chars() {
-      if c == ch {
-        ctr += 1;
-      }
-    }
+    // for c in string.chars() {
+    //   if c == ch {
+    //     ctr += 1;
+    //   }
+    // }
   
-    if ctr <= max && ctr >= min {
+    // if ctr <= max && ctr >= min {
+    //   valid_count += 1;
+    // }
+
+    // if (string.chars().nth((min-1) as usize).unwrap() == ch) ^ (string.chars().nth((max-1) as usize).unwrap() == ch) {
+    //   valid_count += 1;
+    // }
+
+    if (string.as_bytes()[(min-1) as usize] == ch as u8) ^ (string.as_bytes()[(max-1) as usize] == ch as u8) {
       valid_count += 1;
     }
+
   }
   
   println!("Answer: {}", valid_count);
