2eb1759c244d65e9e071a469991f24617e3283c7
wojexe <21208490+wojexe@users.noreply.github.com>
2020-12-02 15:03:50 +0100

    initial commit

---
+++ solution/target/debug/deps/solution-98d61130688b39a7.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/wojexe/Desktop/work_learn/advent-of-code-20/solution/target/debug/deps/solution-98d61130688b39a7: src/main.rs
+
+/mnt/c/Users/wojexe/Desktop/work_learn/advent-of-code-20/solution/target/debug/deps/solution-98d61130688b39a7.d: src/main.rs
+
+src/main.rs:
---
+++ solution/target/rls/debug/deps/solution-5c87a65ee1c7f5df.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/wojexe/Desktop/work_learn/advent-of-code-20/solution/target/rls/debug/deps/solution-5c87a65ee1c7f5df.rmeta: src/main.rs
+
+/mnt/c/Users/wojexe/Desktop/work_learn/advent-of-code-20/solution/target/rls/debug/deps/solution-5c87a65ee1c7f5df.d: src/main.rs
+
+src/main.rs:
