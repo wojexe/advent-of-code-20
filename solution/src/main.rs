64dacd09cb637f5ca2df9f106d09d672e374dfe2
wojexe <21208490+wojexe@users.noreply.github.com>
2020-12-02 22:56:43 +0100

    Day 2 solution

--- solution/src/main.rs
+++ solution/src/main.rs
@@ -1,3 +1,38 @@
+use text_io::scan;
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+  let stdin = io::stdin();
+
+  let mut valid_count: u16 = 0;
+
+  let mut min: u16 = 0;
+  let mut max: u16 = 0;
+  let mut ch: char = '0';
+  let mut string: String = String::new();
+
+  for line in stdin.lock().lines() {
+
+    let l = line.expect("unexpected string");
+
+    if l.is_empty() {
+      break;
+    }
+    
+    scan!(l.bytes() => "{}-{} {}: {}", min, max, ch, string);
+
+    let mut ctr: u16 = 0;
+  
+    for c in string.chars() {
+      if c == ch {
+        ctr += 1;
+      }
+    }
+  
+    if ctr <= max && ctr >= min {
+      valid_count += 1;
+    }
+  }
+  
+  println!("Answer: {}", valid_count);
 }
