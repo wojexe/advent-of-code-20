07dcaf7607858ce39bd9680b0e9f0e6684c6ac40
wojexe <21208490+wojexe@users.noreply.github.com>
2020-12-06 02:56:51 +0100

    Day 4 solution (second star)

--- solution/src/main.rs
+++ solution/src/main.rs
@@ -1,4 +1,5 @@
 use text_io::scan;
+// use colour::*;
 use std::io::{self, BufRead};
 
 /*
@@ -32,20 +33,29 @@ fn main() {
 
             temp_str.split_whitespace().for_each(|x| {
                 let k: String;
-                let v: String;
+                let mut v: String;
                 scan!(x.bytes() => "{}:{}", k, v);
+                // print!("{}: ", k);
+                // green!("{} \t", v);
+                // let prev = current_password_counter;
                 match k.as_str() {
-                    "byr" => current_password_counter += 1,
-                    "iyr" => current_password_counter += 1,
-                    "eyr" => current_password_counter += 1,
-                    "hgt" => current_password_counter += 1,
-                    "hcl" => current_password_counter += 1,
-                    "ecl" => current_password_counter += 1,
-                    "pid" => current_password_counter += 1,
-                    "cid" => (),
+                    "byr" => { if (1920..=2002 as u16).contains(&v.parse::<u16>().unwrap()) { current_password_counter += 1 }; },
+                    "iyr" => { if (2010..=2020 as u16).contains(&v.parse::<u16>().unwrap()) { current_password_counter += 1 }; },
+                    "eyr" => { if (2020..=2030 as u16).contains(&v.parse::<u16>().unwrap()) { current_password_counter += 1 }; },
+                    "hgt" => { let mut ts = String::new(); let mut tn = String::new(); v.chars().for_each(|z| { if z.is_numeric() { tn.push(z) } else if z.is_alphabetic() { ts.push(z)} } ); match ts.as_str() { "in" => if (59..=76).contains(&tn.parse::<u16>().unwrap()) { current_password_counter += 1 }, "cm" => if (150..=193).contains(&tn.parse::<u16>().unwrap()) { current_password_counter += 1 }, _ => {}} },
+                    "hcl" => { if v.len() == 7 && v.as_bytes()[0] == '#' as u8 { v.remove(0); if v.chars().all(char::is_alphanumeric) { current_password_counter += 1 } } },
+                    "ecl" => { if ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].contains(&v.as_str()) { current_password_counter += 1 } },
+                    "pid" => { if v.len() == 9 && v.chars().all(char::is_numeric) { current_password_counter += 1 } },
+                    "cid" => ( ),
                     _ => ()
                 }
+                // if prev == current_password_counter {
+                //     println!("❌");
+                // } else {
+                //     println!("✅");
+                // }
             });
+            // println!("{}\n", current_password_counter);
             
             if current_password_counter >= 7 {
                 valid_password_counter += 1;
