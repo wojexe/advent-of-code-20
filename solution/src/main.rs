15a3277e83c24e937933d306540239d7306663cc
wojexe <21208490+wojexe@users.noreply.github.com>
2020-12-09 15:01:02 +0100

    Day 6 solution

--- solution/src/main.rs
+++ solution/src/main.rs
@@ -1,6 +1,26 @@
+use std::collections::HashSet;
+
 fn main() {
   // get input from file at compiletime
   let input = include_str!("../input.in");
 
-  // solution to the problem
+  let mut v: Vec<HashSet<char>> = Vec::with_capacity(100);
+  v.push(HashSet::new());
+
+  let mut idx: usize = 0;
+
+  for line in input.lines() {
+    if line.is_empty() {
+      idx += 1;
+      v.push(HashSet::new());
+    } else {
+      for ch in line.chars() {
+        v[idx].insert(ch);
+      }
+    }
+  }
+
+  let result = v.iter().fold(0, |acc, map| acc+map.len());
+
+  println!("Answer: {}", result);
 }
