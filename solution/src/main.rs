13e706216d5f2e6d1a6c6ad29641b90a09fdd1d6
wojexe <21208490+wojexe@users.noreply.github.com>
2020-12-09 16:07:34 +0100

    Day 6 solution (second star)

--- solution/src/main.rs
+++ solution/src/main.rs
@@ -1,26 +1,41 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 
 fn main() {
   // get input from file at compiletime
   let input = include_str!("../input.in");
 
-  let mut v: Vec<HashSet<char>> = Vec::with_capacity(100);
-  v.push(HashSet::new());
+  let mut v_m: Vec<HashMap<char, u32>> = Vec::with_capacity(100);
+  v_m.push(HashMap::new());
 
-  let mut idx: usize = 0;
+  let mut i_m: usize = 0;
+  let mut c_lines: u32 = 0;
+  
+  let mut res: u32 = 0;
 
   for line in input.lines() {
     if line.is_empty() {
-      idx += 1;
-      v.push(HashSet::new());
+      for (_, ans_count) in v_m[i_m].iter() {
+        if *ans_count == c_lines {
+          res += 1;
+        }
+      }
+
+      i_m += 1;
+      v_m.push(HashMap::new());
+      c_lines = 0;
     } else {
       for ch in line.chars() {
-        v[idx].insert(ch);
+        *v_m[i_m].entry(ch).or_insert(0) += 1;
       }
+      c_lines += 1;
     }
   }
 
-  let result = v.iter().fold(0, |acc, map| acc+map.len());
+  for (_, ans_count) in v_m[i_m].iter() {
+    if *ans_count == c_lines {
+      res += 1;
+    }
+  }
 
-  println!("Answer: {}", result);
+  println!("Answer: {}", res);
 }
